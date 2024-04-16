cfeba222a9bad7481739a72dd0c1d3a718db17fb
destors <halen54001@gmail.com>
2024-04-16 23:36:41 +0300

    create CRUD actions for opt-products

--- crud-demo/src/app/products-optimistic/state/products-opt.actions.ts
+++ crud-demo/src/app/products-optimistic/state/products-opt.actions.ts
@@ -10,4 +10,31 @@ export namespace ProductsApiActions {
       'Load Products Opts Failure': props<{ error: unknown }>(),
     },
   });
+
+  export const DELETE = createActionGroup({
+    source: 'Products Optimistic',
+    events: {
+      'Load Products Opts': props<{ products: Product }>(),
+      'Load Products Opts Delete Success': props<{ products: Product[] }>(),
+      'Load Products Opts Delete Failure': props<{ error: unknown }>(),
+    },
+  });
+
+  export const UPDATE = createActionGroup({
+    source: 'Products Optimistic',
+    events: {
+      'Load Products Opts': props<{ products: Product }>(),
+      'Load Products Opts Patch Success': props<{ products: Product[] }>(),
+      'Load Products Opts Patch Failure': props<{ error: unknown }>(),
+    },
+  });
+
+  export const CREATE = createActionGroup({
+    source: 'Products Optimistic',
+    events: {
+      'Load Products Opts': props<{ products: Product }>(),
+      'Load Products Opts Create Success': props<{ products: Product[] }>(),
+      'Load Products Opts Create Failure': props<{ error: unknown }>(),
+    },
+  });
 }
